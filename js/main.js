1fbf7b31003abdac2c90a6165ec41d8f19bb1989
Lars de Klijne <43449570+larsdeklijne@users.noreply.github.com>
2019-05-03 00:03:50 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+window.onload = function()
+{
+
+
+
+
+}
+
+function veranderVoortgang(){
+
+  console.log("test");
+  document.getElementById("voortgangsBalk").value = "100";
+}
+
+function foutmelding(){
+  document.getElementById("foutmelding").style.display= "block";
+}
+
+function laadPagina1(){
+
+  location.href = "bestelpagina1.html";
+
+}
+
+function laadPagina2(){
+
+  location.href = "bestelpagina2.html";
+
+}
+
+function laadPagina3(){
+
+  location.href = "bestelpagina3.html";
+
+}
+
+function laadPagina4(){
+
+  location.href = "bestelpagina4.html";
+
+}
+
+function laadPagina5(){
+
+  location.href = "bestelpagina5.html";
+
+}
+
+function laadPagina6(){
+
+  location.href = "bestelpagina6.html";
+
+}
